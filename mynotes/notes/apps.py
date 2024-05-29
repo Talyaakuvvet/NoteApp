85911d2518a5ed1cec2431f580b4b2337b721542
talya kuvvet <talyakuvvet04@gmail.com>
2024-05-29 11:40:02 +0300

    Initial commit

---
+++ mynotes/notes/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NotesConfig(AppConfig):
+    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    name = 'notes'
+    
---
+++ mynotes/static/js/index.js
@@ -0,0 +1,77 @@
+//static/js/index.js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is working!");
+    const btnEl=document.getElementById("btn")
+const appEl=document.getElementById("app")
+btnEl.addEventListener("click",addNote)
+
+getNotes().forEach((note)=>{
+    const noteEl=createNoteEl(note.id, note.content)
+    appEl.insertBefore(noteEl, btnEl);
+})
+
+
+function createNoteEl(id,content){
+    const element = document.createElement("textarea")
+    element.classList.add("note")
+    element.placeholder="Empty note"
+    element.value=content
+    console.log("it is working")
+
+    element.addEventListener("dblclick", ()=>{
+        const warning = confirm("do you want to delete this note?")
+        if(warning){
+            deleteNote(id,element)
+        }
+    })
+    element.addEventListener("input", ()=>{
+        updateNote(id, element.value);
+    })
+    return element;
+}
+
+function deleteNote(id,element){
+    const notes= getNotes().filter((note)=>note.id != id);
+    saveNote(notes);
+    appEl.removeChild(element);
+
+}
+
+
+
+function updateNote(id,content){
+    const notes = getNotes();
+    const target = notes.filter((note)=>note.id==id)[0];
+    target.content=content;
+    saveNote(notes);
+
+}
+
+function addNote(){
+    const notes= getNotes();
+    const noteObj={
+        id: Math.floor(Math.random()*100000), //creating random id and content
+        content:"",
+    };
+    const noteEl=createNoteEl(noteObj.id, noteObj.content)
+    appEl.insertBefore(noteEl, btnEl);
+    
+    notes.push(noteObj);
+    
+    saveNote(notes);
+}
+
+function saveNote(notes){
+    localStorage.setItem("note-app", JSON.stringify(notes));
+}
+
+function getNotes(){
+    return JSON.parse(localStorage.getItem("note-app")||"[]");
+
+}
+
+
+
+
+
+});
